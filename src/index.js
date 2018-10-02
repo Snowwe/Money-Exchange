cf9da391b4b2626ea53745f9de3892078ccf0623
Snezhana <snowwe25@rambler.ru>
2018-10-02 22:15:12 +0300

    version_2

--- src/index.js
+++ src/index.js
@@ -1,32 +1,25 @@
 module.exports = function makeExchange(currency) {
-    if (currency > 10000)
+    if(currency > 10000 ) {
         return {error: "You are rich, my friend! We don't have so much coins for exchange"};
-    else {
-        let h = 0, q = 0, d = 0, n = 0, p = 0;
-        let result = {};
-        h = Math.floor(currency / 50);
-        if (h > 0) {
-            currency = currency - h * 50;
-            result.H = h;
-        }
-        q = Math.floor(currency / 25);
-        if (q > 0) {
-            currency = currency - q * 25;
-            result.Q = q;
-        }
-        d = Math.floor(currency / 10);
-        if (d > 0) {
-            currency = currency - d * 10;
-            result.D = d;
-        }
-        n = Math.floor(currency / 5);
-        if (n > 0) {
-            currency = currency - n * 5;
-            result.N = n;
-        }
-        p = Math.floor(currency / 1);
-        if (p > 0)
-            result.P = p;
-        return result;
     }
+    const currencies = {
+        H: 50,
+        Q: 25,
+        D: 10,
+        N: 5,
+        P: 1
+    };
+
+    let currentCurrency = currency;
+    const result = {};
+
+    for (let key in currencies) {
+        if(currentCurrency >= currencies[key]) {
+            const value = Math.floor(currentCurrency / currencies[key]);
+            result[key] = value;
+            currentCurrency -= value * currencies[key];
+        }
+    }
+
+    return result;
 };
